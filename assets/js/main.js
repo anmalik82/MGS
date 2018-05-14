69c44b215abcccdb0ec5b11d9936cc1c2faef7b2
Ali <letslearntocode@mail.com>
2018-05-14 13:56:03 +0000

    Added typed.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,40 @@
-$(document).ready(function() {
-  $("#toggle").click(function() {
-    var text = $("#toggle").view();
-    if (text == "Read More") {
-      //Stuff to do when btn is in the read more state
-      $("#toggle").view("Read Less");
-      $("#view").slideDown(1000);
-    } else {
-      //Stuff to do when btn is in the read less state
-      $("#toggle").view("Read More");
-      $("#view").slideUp(1000);
-    }
-  });
+// below code is taken from typed.js
+
+var typed = new Typed('.move', {
+  strings: ["Hello World!", "Are you ready to explore the world?"],
+  typeSpeed: 100
 });
+
+var typed = new Typed('.speed', {
+  strings: ["Hello World!", "Are you ready to explore the world?"],
+  typeSpeed: 100
+});
+
+var typed = new Typed('.action', {
+  strings: ["Hello World!", "Are you ready to explore the world?"],
+  typeSpeed: 100
+});
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//   $("#toggle").click(function() {
+//     var text = $("#toggle").view();
+//     if (text == "Read More") {
+//       //Stuff to do when btn is in the read more state
+//       $("#toggle").view("Read Less");
+//       $("#view").slideDown(1000);
+//     } else {
+//       //Stuff to do when btn is in the read less state
+//       $("#toggle").view("Read More");
+//       $("#view").slideUp(1000);
+//     }
+//   });
+// });
+
+
