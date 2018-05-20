b85d879ae5c67de458ad1882810567891a256a3d
Ali <letslearntocode@mail.com>
2018-05-20 20:42:07 +0000

    added twentytwenty.js and new content

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,41 @@ var typed = new Typed('.action', {
 });
 
 
+//active class
+
+// (function($) {
+//     "use strict"; // Start of use strict
+
+//     // jQuery for page scrolling feature - requires jQuery Easing plugin
+//     $(document).on('click', 'a.page-scroll', function(event) {
+//         var $anchor = $(this);
+//         $('html, body').stop().animate({
+//             scrollTop: ($($anchor.attr('href')).offset().top - 30)
+//         }, 800, 'easeInOutExpo');
+//         event.preventDefault();
+//     });
+
+//     // Highlight the top nav as scrolling occurs
+//     $('body').scrollspy({
+//         target: '.navbar-fixed-top',
+//         offset: 100
+//     });
+
+//     // Closes the Responsive Menu on Menu Item Click
+//     $('.navbar-collapse ul li a').click(function() {
+//         $('.navbar-toggler:visible').click();
+//     });
+
+//     // Offset for Main Navigation
+//     $('#navbar_mgs').affix({
+//         offset: {
+//             top: 50
+//         }
+//     })
+
+// })(jQuery); // End of use strict
+
+
 
 //Slidetoggle *****Home Page******
 
@@ -33,6 +68,14 @@ $(document).ready(function(){
             $('#span3').slideToggle('1000');      
   });
   
+});
+
+
+
+// pictures slide
+
+$(window).load(function() {
+  $(".half").twentytwenty();
 });
 
 
