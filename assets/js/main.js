c40f17eeca0a5d4aad17f55990683cab2207d2ad
Ali <letslearntocode@mail.com>
2018-08-10 14:47:03 +0000

    library code files moved to js_library folder

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// carousel typed.js
+  // carousel library code typed.js
 
 for (let selector of ['.move', '.speed', '.action']) { 
     var typed = new Typed(selector,  { 
@@ -8,12 +8,9 @@ for (let selector of ['.move', '.speed', '.action']) {
 }
 
 
-
-
-
-//Slidetoggle
-
 $(document).ready(function(){
+  
+  //Slidetoggle on Home page
 
   $("#button_this1").click(function(){ 
             $('#span1').slideToggle('1000');      
@@ -27,21 +24,27 @@ $(document).ready(function(){
             $('#span3').slideToggle('1000');      
   });
   
+  // active class for navbar
+
+  function makeActive(elem) {
+    $(elem).addClass('active');
+    $(elem).siblings().removeClass('active');
+  }
+  
+  
+  // Animations on scroll waypoints library code 
+  
+  $('.scroll').waypoint(function(direction) {
+    $('.scroll').addClass('animated fadeIn');
+  }, {
+    offset: '80%'
+  });
+  
+  
 });
 
 
-
-
-
-// active class for navbar
-
-function makeActive(elem) {
-  $(elem).addClass('active');
-  $(elem).siblings().removeClass('active');
-}
-
-
-// Google Maps
+  // Library code for Google Maps
 
 function initMap() {
     var myLatLng = {lat: 53.213548, lng: 5.800738};
